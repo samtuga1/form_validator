07fa460835cff2a128beee365364e9055d360bc2
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-01-01 08:23:22 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:login_screen/screens/login_screen.dart';
+
+void main() => runApp(const MaterialApp(
+      home: LoginScreen(),
+    ));
---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class LoginScreen extends StatefulWidget {
+  static String id = 'login_screen';
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
